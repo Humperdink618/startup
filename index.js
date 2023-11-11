b765e077b758dde9124e8fb46dc89483c6d00a9b
Anton K. Anderson <Antongrozny@gmail.com>
2023-11-11 13:31:04 -0700

    back end server startup works probably

--- index.js
+++ index.js
@@ -14,6 +14,48 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// get/post /count
+apiRouter.get('/count', (_req, res) => {
+  console.log("Counts " + counts);
+  res.send(JSON.stringify(counts));
+});
+
+apiRouter.post('/count/:id', (req, res) => {
+  // for front end:  
+  // XXXX.post('/count/' + newCount)
+  let newCount = parseInt(req.params.id);
+  counts = newCount;
+  console.log("Counts " + counts);
+  res.sendStatus(200);
+});
+
+
+// get/post /highscore
+apiRouter.get('/highscore', (_req, res) => {
+  console.log("High score " + highscore);
+  res.send(JSON.stringify(highscore));
+});
+
+apiRouter.post('/highscore', (req, res) => {
+  highscore = parseInt(req.body.newHighscore);
+  console.log("High score " + highscore);
+  res.sendStatus(200);
+});
+
+// Helper functions and stored variables:
+let counts = 0; // initial value
+let highscore = 0; // initial value
+/*
 // GetScores
 apiRouter.get('/scores', (_req, res) => {
   res.send(scores);
@@ -25,14 +67,6 @@ apiRouter.post('/score', (req, res) => {
   res.send(scores);
 });
 
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
 
 // updateScores considers a new score for inclusion in the high scores.
 // The high scores are saved in memory and disappear whenever the service is restarted.
@@ -57,3 +91,4 @@ function updateScores(newScore, scores) {
 
   return scores;
 }
+*/
--- public/play.js
+++ public/play.js
@@ -10,7 +10,12 @@ function getPlayerName() {
   // alert("player name obtained");
   let name = localStorage.getItem("userName");
   // Dummy values which will be taken from the database
-  let counter = 5;
+  // if you want to save count else intialize to zero 
+  // get /count
+  // once get a user, might become get /<username>/count
+  let counter = getCounter();
+  // get /highscore
+  // might be easier to have a highscore per user
   let highScore = 10;
   // set the values in the html
   document.getElementById("player-name").innerHTML = name;
@@ -18,7 +23,61 @@ function getPlayerName() {
   document.getElementById("high-score").innerHTML = highScore;
 }
 
+async function getCounter() {
+  let c = 0;
+  try {
+    // Get the latest count from the service
+    const response = await fetch('/api/count');
+    c = await response.json();
+
+  } catch {
+    console.log("Error when trying to get count");
+  }
+  return c;
+}
+
+async function postCount(newCount) {
+  try {
+    const response = await fetch('/api/count' + newCount, {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify(newCount),
+    });
+  } catch {
+    console.log("Error when trying to post count");
+  }
+}
+
+async function getHighScore() {
+  let h = 0;
+  try {
+    // Get the latest high score from the service
+    const response = await fetch('/api/highscore');
+    hjson = await response.json();
+    h = parseInt(hjson.highScore);
+
+  } catch {
+    console.log("Error when trying to get highscore");
+  }
+  return h;
+}
+
+async function postHighScore(newHighscore) {
+  try {
+    const response = await fetch('/api/highscore', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify(newHighscore),
+    });
+  } catch {
+    console.log("Error when trying to post highscore");
+  }
+}
+
 function reset() {
+  // if want to save count 
+  // post /count ?
+  postCount(0);
   document.getElementById("count").innerHTML = 0;
 }
 
@@ -26,18 +85,23 @@ function pushNoooButton() {
   let newCount = (+document.getElementById("count").innerHTML) + 1;
   // increment count 
   document.getElementById("count").innerHTML = newCount;
+    // -> if you want this to be save when you log out
+  // -> post /count
+  postCount(newCount);
   // check if count is greater than high score
   if (newCount > document.getElementById("high-score").innerHTML) {
+    // if greater than high score, update high score
    updateHighScore(newCount);
   }
-  // if greater than high score, update high score
+
   // play sound
   playVader();
 }
 
 function updateHighScore(newCount) {
-  // update database
+  // update database  -> post to /highscore
   // set high score to count
+  postHighScore(newCount);
   document.getElementById("high-score").innerHTML = newCount;
 }
 
