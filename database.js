8985eace17871b2a3c885fb1ba5a10aeff14d6e3
Anton K. Anderson <Antongrozny@gmail.com>
2023-11-17 18:07:05 -0700

    started implementation of MongoDB for startup. Created new database and collection. Implementation and contents of collection still WIP. File ,gitignore updated to accomodate for MongoDB. MongoDB installed. Added new favicon for startup.

---
+++ database.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('users');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+async function addScore(score) {
+  const result = await userCollection.insertOne(score);
+  return result;
+}
+//may want to use the MongoDB update console command instead of save or insert.
+
+function getHighScores() {
+  const query = { score: { $gt: 0, $lt: 900 } };
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = userCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = { addScore, getHighScores };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
