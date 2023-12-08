d552f4acf69ebcee69aaf924a563ad2d84d5b6c3
Anton K. Anderson <Antongrozny@gmail.com>
2023-12-08 14:41:42 -0700

    React installed. index.jsx, app.jsx, and app.css created. A copy of the original index.html was made and placed in the old data directory so that original data is not lost. Original index.html edited to React format. React Bootstrap installed. Part 2 of React conversion completed, and committed.

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className='body bg-dark text-light'>App will display here</div>;
+}
