d4c515c176971c370a288f37537450dfb21c7dc4
Anton K. Anderson <Antongrozny@gmail.com>
2023-12-13 19:52:31 -0700

    Converted startup folder to react folder.Edited unnecessary assets folder and moved old files to separate directory. Let's hope this fixes any issues.

--- src/play/vaderNOButton.jsx
+++ src/play/vaderNOButton.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-
-
 export function VaderNOButton() {
-    
     return ( 
         <div className="nobutton">
             <img src="button.jpg" alt="NoButton" width='100%'/>
-            <button className="btn" onClick={() => PushNoooButton()}></button>
+            <button className="btn" onClick="pushNoooButton()"></button>
     
         </div>
     )
